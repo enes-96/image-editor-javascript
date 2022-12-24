9deebf5e5494a774e1fe6d7bff6366070f79d300
Seyit Enes <seyit7enes@gmail.com>
2022-12-24 23:10:32 +0100

    done

--- script.js
+++ script.js
@@ -3,13 +3,58 @@
 const img = document.getElementById("img");
 
 const blurFilter = document.getElementById("blur-slider");
+const contrastFilter = document.getElementById("contrast-slider");
+const grayFilter = document.getElementById("gray-slider");
+const hueFilter = document.getElementById("hue-slider");
+const invertFilter = document.getElementById("invert-slider");
+const opacityFilter = document.getElementById("opacity-slider");
+const saturateFilter = document.getElementById("saturate-slider");
+const sepiaFilter = document.getElementById("sepia-slider");
+
+const imgBorder = document.getElementById("border-color");
 
 blurFilter.addEventListener("input", (e) => {
-  img.style.filter = `blur(${blurFilter.value}px)`;
+  const blurWrapper = document.querySelector(".blur-wrapper");
+  blurWrapper.style.filter = `blur(${blurFilter.value}px)`;
 });
 
-const contrastFilter = document.getElementById("contrast-slider");
-
 contrastFilter.addEventListener("input", (e) => {
-  img.style.filter = `contrast(${contrastFilter.value}%`;
+  const contrastWrapper = document.querySelector(".contrast-wrapper");
+  contrastWrapper.style.filter = `contrast(${contrastFilter.value}%)`;
+});
+
+grayFilter.addEventListener("input", (e) => {
+  const grayWrapper = document.querySelector(".gray-wrapper");
+  grayWrapper.style.filter = `grayscale(${grayFilter.value}%)`;
+});
+
+hueFilter.addEventListener("input", () => {
+  const hueWrapper = document.querySelector(".hue-wrapper");
+  hueWrapper.style.filter = `hue-rotate(${hueFilter.value}deg)`;
+});
+
+invertFilter.addEventListener("input", () => {
+  const invertWrapper = document.querySelector(".invert-wrapper");
+  invertWrapper.style.filter = `invert(${invertFilter.value}%)`;
+});
+
+opacityFilter.addEventListener("input", () => {
+  const opacityWrapper = document.querySelector(".opacity-wrapper");
+  opacityWrapper.style.filter = `opacity(${opacityFilter.value}%)`;
+});
+
+saturateFilter.addEventListener("input", () => {
+  const saturadeWrapper = document.querySelector(".saturate-wrapper");
+  saturadeWrapper.style.filter = `saturate(${saturateFilter.value})`;
+  console.log(`saturate(${saturateFilter.value})`);
+});
+
+sepiaFilter.addEventListener("input", () => {
+  const sepiaWrapper = document.querySelector(".sepia-wrapper");
+  sepiaWrapper.style.filter = `sepia(${sepiaFilter.value}%)`;
+  console.log(`sepia(${sepiaFilter.value})`);
+});
+
+imgBorder.addEventListener("input", () => {
+  img.style.border = `12px solid ${imgBorder.value}`;
 });
